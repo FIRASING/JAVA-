80201da06919f08a5161d19a16ea8bdddf6accf8
FIRASING <mohamedfiras.bouzouraa@esprit.tn>
2024-01-28 14:34:01 +0100

    first commit

---
+++ src/ZooManagement.java
@@ -0,0 +1,7 @@
+public class ZooManagement {
+    public static void main (String[] args) {
+        int nbrCages=20;
+        String zooName="myZoo";
+        System.out.println(zooName+" contient "+nbrCages+"cages");
+    }
+}
