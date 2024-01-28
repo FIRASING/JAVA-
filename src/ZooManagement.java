00f68462b3c4abe4802595d074c72a5470bf8ba1
FIRASING <mohamedfiras.bouzouraa@esprit.tn>
2024-01-28 14:47:21 +0100

    second commit

--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class ZooManagement {
     public static void main (String[] args) {
-        int nbrCages=20;
-        String zooName="myZoo";
-        System.out.println(zooName+" contient "+nbrCages+"cages");
+
+       /*PARTIE 1
+       int nbrCages=20;
+        string zooName="myZoo";
+        System.out.println(zooName+" contient "+nbrCages+"cages");*/
+        //PARTIE 2
+        int nbrCages;
+        String zooName;
+        System.out.println("donner le nom du zoo");
+        Scanner k=new Scanner(System.in);
+        zooName=k.nextLine();
+        System.out.println("donner le nombres des cages ");
+        nbrCages=k.nextInt();
+        System.out.println(zooName+" contient "+nbrCages+"  cages");
+
+
     }
 }
