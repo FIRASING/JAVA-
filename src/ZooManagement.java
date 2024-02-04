9a290bb6c34c89d51e2df107dd55325f47e8d609
FIRASING <mohamedfiras.bouzouraa@esprit.tn>
2024-02-04 17:52:40 +0100

    third commit

--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -16,7 +16,26 @@ public class ZooManagement {
         System.out.println("donner le nombres des cages ");
         nbrCages=k.nextInt();
         System.out.println(zooName+" contient "+nbrCages+"  cages");
+        /*ZOO myZoo = new ZOO();
+        Animal lion = new Animal();
+        lion.family = "ggg";
+        lion.name = "firas";
+        lion.age = 22;
+myZoo.nbrCages=10;
+myZoo.city="ariana";
+myZoo.name="rtx";
+myZoo.animals[0]=lion;*/
+        ZOO myZoo = new ZOO("hahha", "ariana", 7);
+        Animal lion = new Animal("kkkk", "rrr", 3, true);
+        Animal chien = new Animal("uuiu", "hhk", 5, false);
+        Animal gazelle = new Animal("gggj", "hhppp", 4, false);
 
+        myZoo.displayZoo();
+        System.out.println(myZoo);
+        System.out.println(lion);
+        boolean lionAdded = myZoo.addAnimal(lion);
+        boolean chienAdded = myZoo.addAnimal(chien);
+        boolean gazelleAdded = myZoo.addAnimal(gazelle);
 
     }
 }
