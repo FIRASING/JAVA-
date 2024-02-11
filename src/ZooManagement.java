2d286c243fab50a19155e86c02ecdb2062b03641
FIRASING <mohamedfiras.bouzouraa@esprit.tn>
2024-02-11 13:15:07 +0100

    forth commit

--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -2,12 +2,6 @@ import java.util.Scanner;
 
 public class ZooManagement {
     public static void main (String[] args) {
-
-       /*PARTIE 1
-       int nbrCages=20;
-        string zooName="myZoo";
-        System.out.println(zooName+" contient "+nbrCages+"cages");*/
-        //PARTIE 2
         int nbrCages;
         String zooName;
         System.out.println("donner le nom du zoo");
@@ -16,26 +10,18 @@ public class ZooManagement {
         System.out.println("donner le nombres des cages ");
         nbrCages=k.nextInt();
         System.out.println(zooName+" contient "+nbrCages+"  cages");
-        /*ZOO myZoo = new ZOO();
-        Animal lion = new Animal();
-        lion.family = "ggg";
-        lion.name = "firas";
-        lion.age = 22;
-myZoo.nbrCages=10;
-myZoo.city="ariana";
-myZoo.name="rtx";
-myZoo.animals[0]=lion;*/
         ZOO myZoo = new ZOO("hahha", "ariana", 7);
         Animal lion = new Animal("kkkk", "rrr", 3, true);
         Animal chien = new Animal("uuiu", "hhk", 5, false);
         Animal gazelle = new Animal("gggj", "hhppp", 4, false);
-
         myZoo.displayZoo();
         System.out.println(myZoo);
         System.out.println(lion);
-        boolean lionAdded = myZoo.addAnimal(lion);
-        boolean chienAdded = myZoo.addAnimal(chien);
-        boolean gazelleAdded = myZoo.addAnimal(gazelle);
-
+        myZoo.addAnimal(lion);
+         myZoo.addAnimal(chien);
+        myZoo.addAnimal(gazelle);
+     myZoo.displayAnimals();
+     int h=myZoo.searchAnimal(gazelle);
+        System.out.println(h);
     }
 }
